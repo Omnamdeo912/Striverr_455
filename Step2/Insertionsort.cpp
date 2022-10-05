0f64cbe7d51b1453e6980a317077a88a86b6bd38
Omnamdeo912 <113748990+Omnamdeo912@users.noreply.github.com>
2022-10-05 08:30:25 +0530

    Create Insertionsort.cpp

---
+++ Step2/Insertionsort.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+// C program for insertion sort
+#include <stdio.h>
+#include <math.h>
+
+void insertionSort(int arr[], int n);
+
+
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+// Driver program to test above functions
+int main()
+{
+    int arr[1000],n,T,i;
+
+    scanf("%d",&T);
+
+    while(T--){
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+
+    insertionSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/* The task is to complete insert() which is used 
+   as shown below to implement insertionSort() */
+
+
+    void insert(int arr[], int i)
+    {
+        // Your code here 
+        for (int j=0;j<i;j++)
+        {
+            //cout<<arr[j]<<" ";
+            
+        }
+    }
+    //Function to sort the array using insertion sort algorithm.
+    void insertionSort(int arr[], int n)
+    {
+        //code here
+        
+        for(int i=1;i<n;i++)
+        {   int current = arr[i];
+            int j=i-1;
+            while(j>=0 && arr[j]>current)        //yaha pe current ki jgh mai arr[i] likh raha tha wich was wrong as i=j+1 and agli line me arr[j+1]=arr[j] kar diye to arr[i] value bhi change ho gyi.
+            {
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=current;
+        }
+    }
+
+
+
+
